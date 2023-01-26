f55ce27c84cb6a76c5f08c728656c70e40ad2daf
МС 21 <m.reutsky@mail.ru>
2023-01-26 20:28:56 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,7 @@
 function compareArrays(arr1, arr2) {
-  
+  return arr1.length === arr2.length && arr1.every((element, i) => element === arr2[i]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  return users.filter(item => item.gender === gender).reduce((acc, item, index, arr) => acc + (item.age / arr.length), 0)
 }
