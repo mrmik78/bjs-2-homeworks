92414dd64cbe49e78d02d6b789dc1c3fef74ee2b
mrmik78 <113300825+mrmik78@users.noreply.github.com>
2025-03-15 21:29:38 +0300

    Update task.js

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -4,7 +4,7 @@ class PrintEditionItem {
         this.name = name;
         this.releaseDate = releaseDate;
         this.pagesCount = pagesCount;
-        this._state = 100;
+        this.state = 100;
         this.type = null;
     }
     fix() {
